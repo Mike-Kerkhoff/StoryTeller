21f86d3449beafff6a5715b20b381b0953d7dccf
Mike-Kerkhoff <mike.kerkhoff@gmail.com>
2014-03-18 00:19:17 +0100

    Update

---
+++ source/storyTeller_2D_Graphics/ChapterLoader.java
@@ -0,0 +1,41 @@
+/*
+*@Autor Mike Kerkhoff ©17.03.2014 
+*/
+package storyTeller_2D_Graphics;
+
+import gameStarter_Screens.LoadingScreen;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import storyTeller_2D_Libraries.Reference;
+
+public class ChapterLoader {
+	
+	private BufferedImage chapter;
+
+	public BufferedImage loadImage (String imagePath) {
+			
+	LoadingScreen.setMessage("Loading textures from " + Reference.CHAPTER_LOCATION);
+		
+	try {
+			
+		chapter = ImageIO.read(new File(Reference.CHAPTER_LOCATION + imagePath));
+	
+	return chapter;
+			
+	} catch (IOException e) {
+		
+		System.err.println("Error: IO Exception");
+		
+	}
+	
+	return null;
+	
+	}
+	
+	
+}
---
+++ source/storyTeller_2D_Libraries/Chapters.java
@@ -0,0 +1,12 @@
+/*
+*@Autor Mike Kerkhoff ©17.03.2014 
+*/
+package storyTeller_2D_Libraries;
+
+import java.awt.image.BufferedImage;
+
+public class Chapters {
+
+	public static BufferedImage introA;
+	
+}
