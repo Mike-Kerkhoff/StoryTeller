e2b9b6a7e8371981c1de4b5124c817d3b2f11a7a
Mike-Kerkhoff <mike.kerkhoff@gmail.com>
2014-03-19 12:32:29 +0100

    Update

--- source/storyTeller_2D_Graphics/Camera.java
+++ source/storyTeller_2D_Graphics/Camera.java
@@ -1,11 +1,11 @@
-/*
+/**
 *@Autor Mike Kerkhoff ©2014 
 */
 
 package storyTeller_2D_Graphics;
 
-/*
-* Importierte Bibliotheken und Klassen
+/**
+* Importierte Bibliotheken und Klassen.
 */
 
 import storyTeller_2D_Core.CoreObject;
@@ -13,68 +13,106 @@ import storyTeller_2D_Entities.Player;
 import storyTeller_2D_Libraries.Object_IDs;
 import storyTeller_2D_Main.StoryTeller_2D;
 
+/**
+* Die Klasse 'Camera' dient als Spielkamera für den StoryTeller2D,
+* sodass der Spieler immer im Mittelpunkt des Bildschirms steht.
+*/
+
 public class Camera {
 
-/*
-* Attribute der Klasse 'Camera'	
-*/
+/**
+ * Der Integer 'x' ist die x-Koordinate des Objektes.
+ */
 	
-	private float x;
-	private float y;
+	private int x;
+	
+/**
+* Der Integer 'y' ist die y-Koordinate des Objektes.
+*/	
+	
+	private int y;
+	
+/**
+* Der Player 'player' ist ein Objekt der Klasse 'Player'.
+*/
 	private Player player;
 
-/*
-* Konstruktor der Klasse 'Camera', nimmt eine float 'x' und eine float 'y' 
-* entgegen
+/**
+* Konstruktor der Klasse 'Camera', nimmt einen Integer 'x' und einen Integer 'y' 
+* entgegen.
+* 
+* @param x : die x-Koordinate des Objektes
+* @param y : die y-Koordinate des Objektes
 */
 	
-	public Camera(float x, float y) {
+	public Camera(int x, int y) {
 		
 		this.x = x;
 		this.y = y;
 		
 		for (CoreObject obj : StoryTeller_2D.getInstance().getController().getObjects()) {
+			
 			if (obj.getID() == Object_IDs.PLAYER) {
+				
 				player = (Player) obj;
+				
 			}
+			
 		}
 		
 	}
 	
-/*
-* Gibt 'x' zurück	
+/**
+* Die Methode 'getX' gibt die x-Koordinate des Objektes zurück.
+* 
+* @return x : die x-Koordinate des Objektes
 */
 
-	public float getX() {
+	public int getX() {
+		
 		return x;
+		
 	}
 
-/*
-* Gibt 'y' zurück	
+/**
+* Die Methode 'getY' gibt die y-Koordinate des Objektes zurück.
+* 
+* @return y : die y-Koordinate des Objektes
 */
 	
-	public float getY() {
+	public int getY() {
+		
 		return y;
+		
 	}
 
-/*
-* Nimmt eine float 'x' entgegen und übernimmt diese	
+/**
+* Die Methode 'setX' nimmt einen Wert für 'x' entgegen und übernimmt diesen.
+* 
+* @param x : die x-Koordinate des Objektes
 */
 	
-	public void setX(float x) {
+	public void setX(int x) {
+		
 		this.x = x;
+		
 	}
 
-/*
-* Nimmt eine float 'y' entgegen und übernimmt diese	
+/**
+* Die Methode 'setY' nimmt einen Wert für 'y' entgegen und übernimmt diesen.
+* 
+* @param y : die y-Koordinate des Objektes
 */	
 	
-	public void setY(float y) {
+	public void setY(int y) {
+		
 		this.y = y;
+		
 	}
 	
-/*
-* Tick-Methode	
+/**
+* Die Methode 'tick' updated die Position der Kamera und setzt diese mit der 
+* Position des Spielers gleich. 
 */
 	
 	public void tick() {
@@ -82,9 +120,5 @@ public class Camera {
 		x += ((-player.getX() + StoryTeller_2D.WIDTH / 2) - x) * 0.0275f;
 		y = (-player.getY() + StoryTeller_2D.HEIGHT / 2);
 	}
-	
-/*
-* Ende der Klasse 'Camera'
-*/
 	
 }
